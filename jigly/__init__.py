7d6c9f445c4c95b8cf4084bf8d36b60032349b1a
mirmik <mirmikns@yandex.ru>
2019-03-25 20:23:32 +0300

    'init'

--- jigly/__init__.py
+++ jigly/__init__.py
@@ -1,2 +1,15 @@
 import os
+import socket
 
+testlist = {
+	"nier": [
+		"nier.mp3",
+		"nier2.mp3",
+	] 
+}
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while 1:
+	sock.recvfrom()
